b0816cf8c2e8d6fa0ab8a22bb412b7a57a198ab2
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2024-06-23 20:24:16 +0530

    The date-time conversion libraries.
    The test here for now is a sanity check that verifies the output of the function with the output from the native win32 calls.
    
    Due to the dependency on Win32 APIs, the test is therefore Windows-only.

---
+++ test/dos_date_time_test.dart
@@ -0,0 +1,57 @@
+@TestOn("windows")
+
+import 'dart:ffi';
+
+import 'package:file_metadata/src/util/dos_date_time.dart';
+import 'package:test/test.dart';
+import 'package:win32/win32.dart';
+import 'package:ffi/ffi.dart';
+
+void main() {
+  test('Misc', () {
+    Pointer<FILETIME> fileTime = calloc<FILETIME>();
+    Pointer<SYSTEMTIME> systemTime = calloc<SYSTEMTIME>();
+    // 09:08:32
+    // int time = 0x1049;
+    int time = 0x4910;
+    // 06-06-2024
+    // int date = 0xC658;
+    int date = 0x58C6;
+
+    DateTime? win32DateTime;
+
+    try {
+      int status = DosDateTimeToFileTime(date, time, fileTime);
+
+      if (status == 0) {
+        print("Error!");
+      } else {
+        status = FileTimeToSystemTime(fileTime, systemTime);
+
+        if (status == 0) {
+          print("Error!");
+        } else {
+          SYSTEMTIME t = systemTime.ref;
+          win32DateTime = DateTime(
+            t.wYear,
+            t.wMonth,
+            t.wDay,
+            t.wHour,
+            t.wMinute,
+            t.wSecond,
+            t.wMilliseconds,
+          );
+          print(win32DateTime);
+        }
+      }
+    } finally {
+      free(systemTime);
+      free(fileTime);
+    }
+
+    DateTime dartDateTime = DosDateTime.fromInt(date, time);
+    print(dartDateTime);
+
+    expect(dartDateTime, equals(win32DateTime));
+  });
+}
