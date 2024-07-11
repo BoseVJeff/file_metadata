4adef2df72828421a4aea99d19e5cec1e3ac1359
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2024-07-11 23:08:19 +0530

    Added methds to read ints and floats in big-endian order.
    This can be essential for some formats that are stored in that form.
    
    Note that these may evolve in the future to have an argument that accepts the endianness.
    
    The existing, non-suffixed methods will continue to use the host endianness, but may be deprecrated in the future in favour of these newer methods.

--- lib/src/util/random_read_file.dart
+++ lib/src/util/random_read_file.dart
@@ -5,6 +5,7 @@ export 'random_read_file/random_read_file_no_io.dart'
 
 // Utility
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'random_read_file/random_read_file_no_io.dart'
     if (dart.library.io) 'random_read_file/random_read_file_io.dart';
@@ -24,11 +25,23 @@ extension TypedRandomRead on RandomReadFile {
   /// Note that this advances the file position.
   Future<int> readUint8() async => (await read(1)).buffer.asUint8List().single;
 
+  /// Read an 8 bit unsigned integer from the file in big-endian order.
+  ///
+  /// Note that this advances the file position.
+  Future<int> readUint8BE() async =>
+      ByteData.sublistView(await read(1)).getUint8(0);
+
   /// Read an 16 bit unsigned integer from the file.
   ///
   /// Note that this advances the file position.
   Future<int> readUint16() async =>
       (await read(2)).buffer.asUint16List().single;
+
+  /// Read an 16 bit unsigned integer from the file in big-endian order.
+  ///
+  /// Note that this advances the file position.
+  Future<int> readUint16BE() async =>
+      ByteData.sublistView(await read(2)).getUint16(0, Endian.big);
 
   /// Read an 32 bit unsigned integer from the file.
   ///
@@ -36,11 +49,23 @@ extension TypedRandomRead on RandomReadFile {
   Future<int> readUint32() async =>
       (await read(4)).buffer.asUint32List().single;
 
+  /// Read an 32 bit unsigned integer from the file in big-endian order.
+  ///
+  /// Note that this advances the file position.
+  Future<int> readUint32BE() async =>
+      ByteData.sublistView(await read(4)).getUint32(0, Endian.big);
+
   /// Read an 64 bit unsigned integer from the file.
   ///
   /// Note that this advances the file position.
   Future<int> readUint64() async =>
       (await read(8)).buffer.asUint64List().single;
+
+  /// Read an 64 bit unsigned integer from the file in big-endian order.
+  ///
+  /// Note that this advances the file position.
+  Future<int> readUint64BE() async =>
+      ByteData.sublistView(await read(8)).getUint64(0, Endian.big);
 
   // Signed integers
   // MARK: Int
@@ -50,20 +75,44 @@ extension TypedRandomRead on RandomReadFile {
   /// Note that this advances the file position.
   Future<int> readInt8() async => (await read(1)).buffer.asInt8List().single;
 
+  /// Read an 8 bit signed integer from the file in big-endian order.
+  ///
+  /// Note that this advances the file position.
+  Future<int> readInt8BE() async =>
+      ByteData.sublistView(await read(1)).getInt8(0);
+
   /// Read an 16 bit signed integer from the file.
   ///
   /// Note that this advances the file position.
   Future<int> readInt16() async => (await read(2)).buffer.asInt16List().single;
+
+  /// Read an 16 bit signed integer from the file in big-endian order.
+  ///
+  /// Note that this advances the file position.
+  Future<int> readInt16BE() async =>
+      ByteData.sublistView(await read(16)).getInt16(0, Endian.big);
 
   /// Read an 32 bit signed integer from the file.
   ///
   /// Note that this advances the file position.
   Future<int> readInt32() async => (await read(4)).buffer.asInt32List().single;
 
+  /// Read an 32 bit signed integer from the file in big-endian order.
+  ///
+  /// Note that this advances the file position.
+  Future<int> readInt32BE() async =>
+      ByteData.sublistView(await read(4)).getInt32(0, Endian.big);
+
   /// Read an 64 bit signed integer from the file.
   ///
   /// Note that this advances the file position.
   Future<int> readInt64() async => (await read(8)).buffer.asInt64List().single;
+
+  /// Read an 64 bit signed integer from the file in big-endian order.
+  ///
+  /// Note that this advances the file position.
+  Future<int> readInt64BE() async =>
+      ByteData.sublistView(await read(8)).getInt64(0, Endian.big);
 
   // Floating point numbers
   // MARK: Floats
@@ -74,11 +123,23 @@ extension TypedRandomRead on RandomReadFile {
   Future<double> readFloat32() async =>
       (await read(4)).buffer.asFloat32List().single;
 
+  /// Read a 32 bit floating point number from the file in big-endian order.
+  ///
+  /// Note that this advances the file position.
+  Future<double> readFloat32BE() async =>
+      ByteData.sublistView(await read(4)).getFloat32(0, Endian.big);
+
   /// Read a 64 bit floating point number from the file.
   ///
   /// Note that this advances the file position.
   Future<double> readFloat64() async =>
       (await read(8)).buffer.asFloat64List().single;
+
+  /// Read a 64 bit floating point number from the file in big-endian order.
+  ///
+  /// Note that this advances the file position.
+  Future<double> readFloat64BE() async =>
+      ByteData.sublistView(await read(8)).getFloat64(0, Endian.big);
 
   // Strings
   // MARK: Strings
