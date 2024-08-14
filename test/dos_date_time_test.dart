2a546f77de1ee08474b495ee587b2e47bcb54e17
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2024-08-14 19:11:04 +0530

    The datetime parsing using win32 has been moved to a seperate function.
    It still remains in the test file as it should not be used or imported anywhere else.

--- test/dos_date_time_test.dart
+++ test/dos_date_time_test.dart
@@ -9,8 +9,6 @@ import 'package:ffi/ffi.dart';
 
 void main() {
   test('Misc', () {
-    Pointer<FILETIME> fileTime = calloc<FILETIME>();
-    Pointer<SYSTEMTIME> systemTime = calloc<SYSTEMTIME>();
     // 09:08:32
     // int time = 0x1049;
     int time = 0x4910;
@@ -18,40 +16,56 @@ void main() {
     // int date = 0xC658;
     int date = 0x58C6;
 
-    DateTime? win32DateTime;
-
-    try {
-      int status = DosDateTimeToFileTime(date, time, fileTime);
-
-      if (status == 0) {
-        throw Exception("Failed to convert to FileTime!");
-      } else {
-        status = FileTimeToSystemTime(fileTime, systemTime);
-
-        if (status == 0) {
-          throw Exception("Failed to convert to SystemTime!");
-        } else {
-          SYSTEMTIME t = systemTime.ref;
-          win32DateTime = DateTime(
-            t.wYear,
-            t.wMonth,
-            t.wDay,
-            t.wHour,
-            t.wMinute,
-            t.wSecond,
-            t.wMilliseconds,
-          );
-          // print(win32DateTime);
-        }
-      }
-    } finally {
-      free(systemTime);
-      free(fileTime);
-    }
+    DateTime? win32DateTime = win32DateTimeFromInt(date, time);
 
     DateTime dartDateTime = DosDateTime.fromInt(date, time);
     // print(dartDateTime);
 
     expect(dartDateTime, equals(win32DateTime));
   });
+
+  test('DOS Timestamp', () {
+    expect(
+      DosDateTime.fromTimestamp(0x665b12d2),
+      equals(DateTime.utc(2024, 06, 01, 12, 23, 46)),
+    );
+  });
+}
+
+DateTime? win32DateTimeFromInt(int date, int time) {
+  Pointer<FILETIME> fileTime = calloc<FILETIME>();
+  Pointer<SYSTEMTIME> systemTime = calloc<SYSTEMTIME>();
+
+  DateTime? win32DateTime;
+
+  try {
+    int status = DosDateTimeToFileTime(date, time, fileTime);
+
+    if (status == 0) {
+      throw Exception("Failed to convert to FileTime!");
+    } else {
+      status = FileTimeToSystemTime(fileTime, systemTime);
+
+      if (status == 0) {
+        throw Exception("Failed to convert to SystemTime!");
+      } else {
+        SYSTEMTIME t = systemTime.ref;
+        win32DateTime = DateTime(
+          t.wYear,
+          t.wMonth,
+          t.wDay,
+          t.wHour,
+          t.wMinute,
+          t.wSecond,
+          t.wMilliseconds,
+        );
+        // print(win32DateTime);
+      }
+    }
+  } finally {
+    free(systemTime);
+    free(fileTime);
+  }
+
+  return win32DateTime;
 }
